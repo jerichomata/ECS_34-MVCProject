ea81ca9a58f47eabf921c592f5dc1ad5e5b518ae
jerichomata <74013719+jerichomata@users.noreply.github.com>
2022-02-19 18:23:34 -0800

    commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from random import randint
 
-for i in range (266, 270):
+for i in range (272, 280):
     d = str(i) + ' days ago'
     with open('file.txt', 'a') as file:
         file.write(d)
